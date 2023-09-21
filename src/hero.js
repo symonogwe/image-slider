169e82a68a6bbc3cdc03f18e7b1b30710f12a540
Symon Ogwe <symonogwe@gmail.com>
2023-09-21 21:57:24 +0300

    Implement next image functionality in hero.js
    - Clicking on the next btn scrolls to the next image

--- src/hero.js
+++ src/hero.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import previousIconSrc from "./Assets/previous.svg";
 import nextIconSrc from "./Assets/next.svg";
 
@@ -7,22 +8,6 @@ import deerSrc from "./Assets/deer.jpg";
 import elephantSrc from "./Assets/elephant.jpg";
 import rhinoSrc from "./Assets/rhino.jpg";
 
-// Hero element
-const heroElement = document.getElementById("hero-section");
-const computedStyle = window.getComputedStyle(heroElement);
-const backgroundImage = computedStyle.getPropertyValue("background");
-
-const urlRegex = /url\(['"]?([^'"]+)['"]?\)/;
-const match = backgroundImage.match(urlRegex);
-const decoded = decodeURIComponent(match[1]); // decoded now matches url
-
-// Slider nav Icons
-const previousIcon = document.querySelector(".previous-icon");
-previousIcon.src = previousIconSrc;
-
-const nextIcon = document.querySelector(".next-icon");
-nextIcon.src = nextIconSrc;
-
 // Hero src
 const lioness = lionessSrc;
 const bear = bearSrc;
@@ -31,3 +16,45 @@ const elephant = elephantSrc;
 const rhino = rhinoSrc;
 
 const heroSrcArray = [lioness, bear, deer, elephant, rhino];
+
+const heroClassArray = [
+  "lioness-src",
+  "bear-src",
+  "deer-src",
+  "elephant-src",
+  "rhino-src",
+];
+
+// Next Image functionality
+function nextImage() {
+  console.log("hello");
+  const heroElement = document.getElementById("hero-section");
+  const computedStyle = window.getComputedStyle(heroElement);
+  const backgroundImage = computedStyle.getPropertyValue("background");
+
+  const urlRegex = /url\(['"]?([^'"]+)['"]?\)/;
+  const match = backgroundImage.match(urlRegex);
+  const currentImgSrc = decodeURIComponent(match[1]);
+  console.log(currentImgSrc);
+
+  if (currentImgSrc === heroSrcArray[4]) {
+    heroElement.classList.remove(heroClassArray[4]);
+    heroElement.classList.add(heroClassArray[0]);
+  } else {
+    for (let i = 0; i < 4; i++) {
+      if (currentImgSrc === heroSrcArray[i]) {
+        heroElement.classList.remove(heroClassArray[i]);
+        heroElement.classList.add(heroClassArray[i + 1]);
+      }
+    }
+  }
+}
+
+const nextIcon = document.querySelector(".next-icon");
+nextIcon.src = nextIconSrc;
+
+nextIcon.addEventListener("click", nextImage);
+
+// previous Image functionality
+const previousIcon = document.querySelector(".previous-icon");
+previousIcon.src = previousIconSrc;
