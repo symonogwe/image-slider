0d89798845996fe363ec170b7e3a033e53b6b5fa
Symon Ogwe <symonogwe@gmail.com>
2023-09-21 15:31:03 +0300

    Position next & previous icons
    - Assign their src using javascript

---
+++ src/hero.js
@@ -0,0 +1,9 @@
+import previousIconSrc from "./Assets/previous.svg";
+import nextIconSrc from "./Assets/next.svg";
+
+// Slider nav Icons
+const previousIcon = document.querySelector(".previous-icon");
+previousIcon.src = previousIconSrc;
+
+const nextIcon = document.querySelector(".next-icon");
+nextIcon.src = nextIconSrc;
