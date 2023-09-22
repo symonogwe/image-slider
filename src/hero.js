c305b8822d8f5226805c106421aa519bf3c9f16a
Symon Ogwe <symonogwe@gmail.com>
2023-09-22 08:58:01 +0300

    Implement previous image functionality in hero.js

--- src/hero.js
+++ src/hero.js
@@ -26,7 +26,6 @@ const heroClassArray = [
 
 // Next Image functionality
 function nextImage() {
-  console.log("hello");
   const heroElement = document.getElementById("hero-section");
   const computedStyle = window.getComputedStyle(heroElement);
   const backgroundImage = computedStyle.getPropertyValue("background");
@@ -34,7 +33,6 @@ function nextImage() {
   const urlRegex = /url\(['"]?([^'"]+)['"]?\)/;
   const match = backgroundImage.match(urlRegex);
   const currentImgSrc = decodeURIComponent(match[1]);
-  console.log(currentImgSrc);
 
   if (currentImgSrc === heroSrcArray[4]) {
     heroElement.classList.remove(heroClassArray[4]);
@@ -55,8 +53,32 @@ nextIcon.src = nextIconSrc;
 nextIcon.addEventListener("click", nextImage);
 
 // previous Image functionality
+function previousImage() {
+  const heroElement = document.getElementById("hero-section");
+  const computedStyle = window.getComputedStyle(heroElement);
+  const backgroundImage = computedStyle.getPropertyValue("background");
+
+  const urlRegex = /url\(['"]?([^'"]+)['"]?\)/;
+  const match = backgroundImage.match(urlRegex);
+  const currentImgSrc = decodeURIComponent(match[1]);
+
+  if (currentImgSrc === heroSrcArray[0]) {
+    heroElement.classList.remove(heroClassArray[0]);
+    heroElement.classList.add(heroClassArray[4]);
+  } else {
+    for (let i = 1; i <= 4; i++) {
+      if (currentImgSrc === heroSrcArray[i]) {
+        heroElement.classList.remove(heroClassArray[i]);
+        heroElement.classList.add(heroClassArray[i - 1]);
+      }
+    }
+  }
+}
+
 const previousIcon = document.querySelector(".previous-icon");
 previousIcon.src = previousIconSrc;
+
+previousIcon.addEventListener("click", previousImage);
 
 // functionality to automatically change image
 const autoSlide = setInterval(nextImage, 5000);
